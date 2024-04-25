406cc64f319fd7f233f9f3cb483f587936a2261f
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-04-25 19:06:22 +0300

    Мелочевка

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    /*public InventorySlot[] InventorySlots;
+    private int _selectedSlot = -1;
+
+    private void ChangeSelectedSlot(int newValue)
+    {
+        if(_selectedSlot >= 0)
+        {
+            InventorySlots[_selectedSlot].Deselect();
+        }
+        
+        InventorySlots[newValue].Select();
+        _selectedSlot = newValue;
+    }*/
+}
--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -2,9 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
+    //public Image Image;
+    //public Color SelectedColor, NotSelectedColor;
+
+   /* private void Awake()
+    {
+        Deselect();
+    }
+    public void Select()
+    {
+        Image.color = SelectedColor;
+    }
+    public void Deselect()
+    {
+        Image.color = NotSelectedColor;
+    }*/
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
