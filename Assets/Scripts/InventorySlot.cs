3f26130b3660b93560a21869ea4681a360a9bbf4
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-04-26 16:46:22 +0300

    Закинул звуки

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -6,22 +6,7 @@ using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    //public Image Image;
-    //public Color SelectedColor, NotSelectedColor;
-
-   /* private void Awake()
-    {
-        Deselect();
-    }
-    public void Select()
-    {
-        Image.color = SelectedColor;
-    }
-    public void Deselect()
-    {
-        Image.color = NotSelectedColor;
-    }*/
-
+    
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+[CreateAssetMenu(menuName = "Scriptable Object/Item")]
+public class Items : ScriptableObject
+{
+    public GameObject[] CraftMaterials;
+    public GameObject[] Potion;
+
+    /*[Header("Only gameplay")]
+    public TileBase Tile;
+    public ItemType Type;
+    public ActionType ActionType;
+    public Vector2Int range = new Vector2Int(5, 4);
+
+    [Header("Only UI")]
+    public bool Stackable = true;
+
+    [Header("Both")]
+    public Sprite Image;*/
+}
+
+/*public enum ItemType
+{
+
+}
+public enum ActionType
+{
+
+}*/
