2f6f8f9ec698b6a12a872e62c630f3e0f1b586d5
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-04-28 18:38:54 +0300

    Фиксы

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -1,12 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    
+
+    //private GameObject childObject;
+    //private GridLayoutGroup gridLayoutGroup;
+
+    /*void Start()
+    {
+        if (transform.childCount > 0)
+        {
+            childObject = transform.GetChild(0).gameObject;
+            gridLayoutGroup = GetComponentInParent<GridLayoutGroup>();
+        }
+    }
+
+    void Update()
+    {
+        if (childObject != null && gridLayoutGroup != null)
+        {
+            if (childObject.CompareTag("CyanPotion2"))
+            {
+                gridLayoutGroup.cellSize = new Vector2(100, 150);
+            }
+            else
+            {
+                gridLayoutGroup.cellSize = new Vector2(180, 180);
+            }
+        }
+    }*/
+
     public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
@@ -15,6 +43,7 @@ public class InventorySlot : MonoBehaviour, IDropHandler
             DraggableItem draggableItem = dropped.GetComponent<DraggableItem>();
             draggableItem.parentAfterDrag = transform;
         }
-        
+
     }
+
 }
--- Assets/Scripts/PotionCreationScript.cs
+++ Assets/Scripts/PotionCreationScript.cs
@@ -6,89 +6,85 @@ using UnityEngine.UI;
 
 public class ObjectCreationScript : MonoBehaviour
 {
-    public GameObject[] Material;
+    //public GameObject[] Material;
     public GameObject[] Potion;
     public InventorySlot[] Slot;
     public AudioSource[] AudioSources;
 
     public void CreateObject()
     {
-        //Vector3 resultPosition = Slot[3].transform.position;
+        if (Slot[0].transform.childCount > 0
+         && Slot[1].transform.childCount > 0
+         && Slot[2].transform.childCount > 0)
+        {
+            if (CompareForCraft("GreenWater", "Eye", "Feather"))
+            {
+                CraftPotionIndex(5);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("PinkPlazma", "Candles", "Gross"))
+            {
+                CraftPotionIndex(2);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("GreenWater", "Spider", "Skull"))
+            {
+                CraftPotionIndex(7);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Gross", "FirePlazma", "EvilGross"))
+            {
+                CraftPotionIndex(10);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Feather", "Bat", "Spider"))
+            {
+                CraftPotionIndex(8);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Spider", "LeavePlazma", "Skull"))
+            {
+                CraftPotionIndex(1);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("HornSkull", "LeavePlazma", "Hand"))
+            {
+                CraftPotionIndex(0);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Skull", "OldGross", "OldGross"))
+            {
+                CraftPotionIndex(4);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Hand", "Lizard", "Horn"))
+            {
+                CraftPotionIndex(9);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Lizard", "FirePlazma", "EvilGross"))
+            {
+                CraftPotionIndex(3);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("Lizard", "Spider", "Hand"))
+            {
+                CraftPotionIndex(6);
+                DeleteMaterials();
+            }
+            else if (CompareForCraft("FirePlazma", "LeavePlazma", "PinkPlazma"))
+            {
+                CraftPotionIndex(11);
+                DeleteMaterials();
+            }
+            else
+            {
+                AudioSources[0].Play();
+                Debug.Log("ErrorSound");
+            }
+        }
+        else AudioSources[0].Play();
 
-        /*if (Slot[0].transform.Find("Bat") &&
-            Slot[1].transform.Find("Candle") &&
-            Slot[2].transform.Find("Candles"))
-
-        {
-            GameObject resultPotion = Instantiate(Potion[0], resultPosition, Quaternion.identity);
-            resultPotion.transform.SetParent(Slot[3].transform);
-        }*/
-
-        if (CompareForCraft("GreenWater", "Eye", "Feather"))
-        {
-            CraftPotionIndex(5);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("PinkPlazma", "Candles", "Gross"))
-        {
-            CraftPotionIndex(2);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("GreenWater", "Spider", "Skull"))
-        {
-            CraftPotionIndex(7);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Gross", "FirePlazma", "EvilGross"))
-        {
-            CraftPotionIndex(10);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Feather", "Bat", "Spider"))
-        {
-            CraftPotionIndex(8);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Spider", "LeavePlazma", "Skull"))
-        {
-            CraftPotionIndex(1);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("HornSkull", "LeavePlazma", "Hand"))
-        {
-            CraftPotionIndex(0);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Skull", "OldGross", "OldGross"))
-        {
-            CraftPotionIndex(4);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Hand", "Lizard", "Horn"))
-        {
-            CraftPotionIndex(9);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Lizard", "FirePlazma", "EvilGross"))
-        {
-            CraftPotionIndex(3);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("Lizard", "Spider", "Hand"))
-        {
-            CraftPotionIndex(6);
-            DeleteMaterials();
-        }
-        else if (CompareForCraft("FirePlazma", "LeavePlazma", "PinkPlazma"))
-        {
-            CraftPotionIndex(11);
-            DeleteMaterials();
-        }
-        else 
-        { 
-            AudioSources[0].Play();
-            Debug.Log("ErrorSound");
-        }
     }
 
     public bool CompareForCraft(string materialTag1, string materialTag2, string materialTag3)
