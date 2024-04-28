35103af3b8941ce04240455ec0148fe11c8b2ff3
Andrey <fav.777@mail.ru>
2024-04-28 23:49:02 +0300

    qwe

--- Assets/Scripts/SpawnResours.cs
+++ Assets/Scripts/SpawnResours.cs
@@ -15,8 +15,16 @@ public class SpawnResours : MonoBehaviour
         {
             _randomRes = Random.Range(0, _resours.Length);
             Vector3 resPosition = Slot.transform.position;
-            GameObject resultPotion = Instantiate(_resours[_randomRes], resPosition, Quaternion.identity);
-            resultPotion.transform.SetParent(Slot.transform);
+            GameObject resultRes = Instantiate(_resours[_randomRes], resPosition, Quaternion.identity);
+            resultRes.transform.SetParent(Slot.transform);
+        }
+    }
+
+    public void DeleteRes()
+    {
+        if (Slot.transform.childCount != 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
