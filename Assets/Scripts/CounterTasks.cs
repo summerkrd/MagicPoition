9989756cd7f805fb13d24e35cec8e3865af9527d
Andrey <fav.777@mail.ru>
2024-04-28 22:45:46 +0300

    qweqe

---
+++ Assets/Scripts/CounterTasks.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CounterTasks : MonoBehaviour
+{
+    public static TMP_Text _ollTasks;
+
+    void Update()
+    {
+        
+    }
+}
